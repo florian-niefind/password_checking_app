4a4a348e7f1e3f32b66840b4526b7f5848266b69
Dr. Florian Niefind <florian@zenguard.org>
2018-05-31 14:42:12 +0200

    Write password checker app
    Correct credentials are hardcoded for now. Hello world route still
    exists. Added tests for credentials checker.

--- src/password_checking_app/handler.clj
+++ src/password_checking_app/handler.clj
@@ -3,8 +3,15 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def credentials {:username "Florian" :password "abcd"})
+
+(defn check-credentials [username password credentials]
+  (if (= username (:username credentials))
+    (if (= password (:password credentials)) "success" "wrong password") "user not found"))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/login" [username password] (check-credentials username password credentials))
   (route/not-found "Not Found"))
 
 (def app
--- test/password_checking_app/handler_test.clj
+++ test/password_checking_app/handler_test.clj
@@ -12,3 +12,20 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest test-credentials-check
+
+  (testing "wrong user"
+    (let [creds {:username "usr" :password "pw"}
+          res (check-credentials "a" "pw" creds)]
+      (is (= res "user not found"))))
+
+  (testing "wrong password"
+    (let [creds {:username "usr" :password "pw"}
+          res (check-credentials "usr" "px" creds)]
+      (is (= res "wrong password"))))
+
+  (testing "success"
+    (let [creds {:username "usr" :password "pw"}
+          res (check-credentials "usr" "pw" creds)]
+      (is (= res "success")))))
